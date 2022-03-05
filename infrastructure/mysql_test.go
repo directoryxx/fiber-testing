c2459d4337a81c4bcb39f4c07526bd86955c9519
directoryx <anggawijaya4567@gmail.com>
2022-03-05 17:47:24 +0700

    testing

--- infrastructure/mysql_test.go
+++ infrastructure/mysql_test.go
@@ -13,7 +13,7 @@ func TestOpenDB(t *testing.T) {
 	errLoadEnv := godotenv.Load(path.Join(os.Getenv("HOME")) + "/goproject/rest-api/.env")
 	//helper.PanicIfError(errLoadEnv)
 	config.GetConfiguration(errLoadEnv)
-	dsn := config.GenerateDSNMySQL(true)
+	dsn := config.GenerateDSNMySQL()
 	db,err := OpenDBMysql(dsn)
 	if assert.NoError(t, err) {
 		assert.NotNil(t, db)
